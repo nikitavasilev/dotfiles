ec51fee3a60fd614a00abc3bee32596efb5ab01b
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-05-04 02:24:00 +0200

    manual inst list

---
+++ list.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo 'Please make sure install at least Nodejs before executing install_apps.sh'
+echo -e '\nClick on the links above to download the executables files:'
+
+echo -e '\nNode.js:'
+echo 'https://nodejs.org/en/'
+
+echo -e '\nVisual Studio Code:'
+echo 'https://code.visualstudio.com/'
+
+echo -e '\nSlack:'
+echo 'https://slack.com/intl/en-fr/downloads/linux'
+
+echo -e '\nDiscord:'
+echo 'https://discordapp.com/download'
+
+echo -e '\nGoogle Chrome:'
+echo 'https://www.google.com/chrome/'
+
+echo -e '\nNordVPN:'
+echo 'https://nordvpn.com/download/'
+
+echo -e '\nSkype:'
+echo 'https://www.skype.com/en/get-skype/download-skype-for-desktop/'
+
+echo -e '\nInstall the next apps via the Deepin App Store:'
+echo 'Kazam\nqBittorrent\nTor Browser\nVLC Media Player\nPopcorn Time\nBleachbit\nSynaptic Package Manager\nPlay on Linux\nVirtualBox\nGIMP\nInkscape\nHardinfo'
+
+echo -e '\nMake sure to set the Region to International instead of China!'
